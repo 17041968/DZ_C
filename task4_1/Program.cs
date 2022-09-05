c762881e55601ef913ca804fdbce0dbf38824b2a
Yuri <1234@mail.ru>
2022-09-05 21:21:07 +0400

    task4_1

---
+++ task4_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ввести начало интервала: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Ввести конец интервала: ");
+int b = int.Parse(Console.ReadLine());
+   for (int i = a; i <= b; i++)
+   {
+      if (i % 2 == 0) 
+      {
+         Console.Write(i + " ");
+      }
+   }
